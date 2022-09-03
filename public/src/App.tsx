b66dc7ccc9590af63f3350d7d986482f91045ee3
Sean <andricoseanwork@gmail.com>
2022-09-03 20:13:43 +0800

    re-using components

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -1,13 +1,12 @@
+import Todo from "./components/Todo";
+
 function App() {
   return (
     <div>
       <h1>TODO</h1>
-      <div className="card">
-        Title
-        <div className="actions">
-          <button className="btn">Delete</button>
-        </div>
-      </div>
+      <Todo />
+      <Todo />
+      <Todo />
     </div>
   );
 }
---
+++ public/src/components/Todo.tsx
@@ -0,0 +1,12 @@
+function Todo() {
+  return (
+    <div className="card">
+      Title
+      <div className="actions">
+        <button className="btn">Delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
